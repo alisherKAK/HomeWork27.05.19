1ad41dde72f96bc060f70da2e341d5477a88f73f
Alisher Kalabaev <alisher.alish333@gmail.com>
2019-05-20 21:20:06 +0600

    Home Work 27.05.19

---
+++ HomeWork27_05_19.WpfApp/HomeWork27_05_19.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace HomeWork27_05_19.WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PrintButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                printDialog.PrintDocument(
+                    ((IDocumentPaginatorSource)document.Document).DocumentPaginator, "");
+            }
+        }
+    }
+}
